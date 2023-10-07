17b3c7f4a999c074f44b7a06ad635fb91a6a4cbf
Salma <salmahossain52@gmail.com>
2023-10-07 11:28:57 -0500

    first commit

---
+++ src/test/java/JavaReverse.java
@@ -0,0 +1,20 @@
+public class JavaReverse {
+
+    public static void main(String[] args) {
+
+        JavaReverse obj = new JavaReverse();
+        obj.paraint(5000, 4000);
+        JavaReverse.para("Saima", " A");
+    }
+
+    private static void para(String firstname, String lastname) {
+
+        String fullname = firstname + lastname;
+        System.out.println(fullname);
+    }
+
+    public void paraint(int a, int b) {
+        int c = a + b;
+        System.out.println(c);
+    }
+}
